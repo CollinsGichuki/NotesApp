c59b8c24952b7f8106719570ff16c19b64314880
Snilloc <collinsgichuki9@gmail.com>
2020-06-29 11:30:18 +0300

    Added the Base Application class and other files that were not added previously

---
+++ app/src/main/java/com/example/android/mvvm/BaseApplication/MyApplication.java
@@ -0,0 +1,34 @@
+package com.example.android.mvvm.BaseApplication;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setAppTheme();
+    }
+
+    private void setAppTheme() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String selectedTheme = sharedPreferences.getString("night_mode_state_key", "");
+        if (!selectedTheme.equals("")){
+            switch (selectedTheme){
+                case "System Default":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                    break;
+                case "Dark Mode":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    break;
+                case "Day Mode":
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    break;
+            }
+        }
+    }
+}
